225563e95ec7f4cb05041a552c244f8ae40d6977
Prado Zuta, Miguel Angel <mikk_pr@hotmail.com>
2023-07-17 15:02:37 -0500

    Layout Personalizado

--- app/src/main/java/com/example/dam_examen_final_prado_zuta/NotaFragment.java
+++ app/src/main/java/com/example/dam_examen_final_prado_zuta/NotaFragment.java
@@ -9,6 +9,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -66,12 +67,19 @@ public class NotaFragment extends Fragment {
             if (mColumnCount <= 1) {
                 recyclerView.setLayoutManager(new LinearLayoutManager(context));
             } else {
-                recyclerView.setLayoutManager(new StaggeredGridLayoutManager( mColumnCount, StaggeredGridLayoutManager.VERTICAL));
+                DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+                float dpWidth = displayMetrics.widthPixels / displayMetrics.density;
+                int numeroColumnas = (int) (dpWidth / 180);
+                recyclerView.setLayoutManager(new StaggeredGridLayoutManager( numeroColumnas, StaggeredGridLayoutManager.VERTICAL));
             }
             notaList = new ArrayList<>();
-            notaList.add(new Nota("UC4","Estudiar para la evaluación de la UC4 - Caso: Notas y Listas", true, android.R.color.holo_blue_light));
-            notaList.add(new Nota("Recordar", "He aparcado el coche en la calle República Argentina, no olvidarme en el parque",false, android.R.color.holo_green_light));
-            notaList.add(new Nota("cumpleaños (fiesta)","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", true, android.R.color.holo_orange_light));
+            notaList.add(new Nota("Trabajo Académico","Las notas del trabajo académico fueron revisadas e ingresadas el día 16 de Julio.", true, android.R.color.holo_blue_light));
+            notaList.add(new Nota("Examen Final", "Estudiar para el Examen Final: Caso: Notas Listas, es una réplica de la UC4",false, android.R.color.holo_green_light));
+            notaList.add(new Nota("Resultado de Examen Final","La Revisión del examen final se revisarán durante la sesión de clases y el resultado será publicado en los próximos días, especificamente: 24/07/23 - 31/07/23  Examen final", true, android.R.color.holo_orange_light));
+            notaList.add(new Nota("Examen Sustitutorio","Examen Sustitutorio planificado para el día 24 de julio del 2023. Asistir puntualmente a todos los que se van a presentar.", true,android.R.color.holo_blue_light));
+            notaList.add(new Nota("Fiestas Patrias", "Desfiles militares, ferias gastronómicas y recorridos turísticos se abren para celebrar en este año 2023. ¡Felíz día del Perú!",false, android.R.color.holo_green_light));
+            notaList.add(new Nota("Despedida de Ciclo","Aprecio mucho su amistad, su participación en las sesiones de clases, cada ciclo es una nueva experiencia, deseo que sigan estudiando. Muchas Felicidades. Nos vemos el 2023 - II", true, android.R.color.holo_orange_light));
+            notaList.add(new Nota("Estudia y superate","Todas las distintas capacidades que el hombre posee de la mente, del alma y del cuerpo le fueron dadas por Dios para que las dedique a alcanzar el más alto grado de excelencia posible. Toda facultad y todo atributo con que el Creador nos haya dotado deben emplearse para su gloria y para el ennoblecimiento de nuestros semejantes. Y en este empleo se halla la ocupación más pura, más noble y más feliz.", true, android.R.color.holo_blue_light));
             adapterNotas = new MyNotaRecyclerViewAdapter(notaList, mListener);
             recyclerView.setAdapter(adapterNotas);
         }
